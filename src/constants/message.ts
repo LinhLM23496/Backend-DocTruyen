dac749515805bb1f8df0b407b88119e5bcf239b9
le linh <linhlm23496@gmail.com>
2024-01-29 21:09:36 +0700

    fix message

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -10,15 +10,15 @@ export const Messages = {
   HTTP_500_INTERNAL_SERVER_ERROR: 'Internal Server Error',
 
   // Các thông điệp khác
-  LOGOUT_SUCCESSFUL: 'Đăng xuất thành công rồi! Chúng tôi sẽ nhớ về bạn đó!',
-  LOGIN_SUCCESSFUL: 'Đăng nhập thành công rồi! Chào mừng bạn quay trở lại!',
+  LOGOUT_SUCCESSFUL: 'Đăng xuất thành công rồi! \nChúng tôi sẽ nhớ về bạn đó!',
+  LOGIN_SUCCESSFUL: 'Đăng nhập thành công rồi! \nChào mừng bạn quay trở lại!',
   DELETE_USER_SUCCESS:
-    'Xóa người dùng thành công. Chúng tôi ghi nhận tất cả đóng góp của bạn cho chúng tôi! Chân thành cảm ơn bạn! <3',
+    'Xóa người dùng thành công. \nChúng tôi ghi nhận tất cả đóng góp của bạn cho chúng tôi! \nChân thành cảm ơn bạn! <3',
   UPDATE_USER_SUCCESS: 'Cập nhật thông tin thành công rồi đó!',
-  UPDATE_PASSWORD_SUCCESS: 'Chúng tôi đổi cho cái mật khẩu mới rồi đó! Hãy nhớ nó nhé!',
+  UPDATE_PASSWORD_SUCCESS: 'Chúng tôi đổi cho cái mật khẩu mới rồi đó! \nHãy nhớ nó nhé!',
   GET_ALL_USERS_SUCCESS: 'Get all users successfully.',
   ACCESS_TOKEN_CREATED: 'Access token created successfully.',
-  GET_USER_INFO_SUCCESS: 'Thông tin người dùng đã được tải về. Hãy nhớ về người đó nhé!',
+  GET_USER_INFO_SUCCESS: 'Thông tin người dùng đã được tải về. \nHãy nhớ về người đó nhé!',
   VALID_REFRESH_TOKEN: 'Valid refresh token.',
   SENDED_CODE_EMAIL: 'Kiểm tra email của bạn để lấy mã nhé!',
 
@@ -26,8 +26,8 @@ export const Messages = {
   GET_ALL_BOOKS_SUCCESS: 'Get all books successfully.',
   GET_BOOK_SUCCESS: 'Get book successfully.',
   BOOK_NOT_EXIST: 'Chúng tôi "CHẢ" thấy truyện này đâu cả!',
-  UPDATE_BOOK_SUCCESS: 'Cập nhật sách thành công. Cập nhật thêm nữa nha!',
-  DELETE_BOOK_SUCCESS: 'Xoá sách thành công. Nhưng mà đừng xoá nữa nha!',
+  UPDATE_BOOK_SUCCESS: 'Cập nhật sách thành công. \nCập nhật thêm nữa nha!',
+  DELETE_BOOK_SUCCESS: 'Xoá sách thành công. \nNhưng mà đừng xoá nữa nha!',
 
   // Chapters
   GET_ALL_CHAPTERS_SUCCESS: 'Get all chapters successfully.',
@@ -41,7 +41,7 @@ export const Messages = {
   GET_SUGGESTION_SUCCESS: 'Get suggestion successfully.',
 
   // Error
-  INVALID_EMAIL_PASSWORD: 'Sai email hoặc mật khẩu rồi bạn ơi! Ráng nhớ lại đi nào!',
+  INVALID_EMAIL_PASSWORD: 'Sai email hoặc mật khẩu rồi bạn ơi! \nRáng nhớ lại đi nào!',
   INVALID_REFRESH_TOKEN: 'Invalid refresh token.',
   ALL_FIELDS_REQUIRED: 'Chúng tôi cần tất cả các trường đó bạn ơi!',
   EMAIL_ALREADY_EXIST: 'Email này đã tồn tại rồi bạn ơi!',
@@ -51,11 +51,11 @@ export const Messages = {
   FIELD_BOOKID_CHAPERID_REQUIRED: 'Chúng tôi cần bookId và chapterId đó, gửi cho chúng tôi đi bạn ơi!',
   FIELD_CHAPTERID_REQUIRED: 'Chúng tôi cần chapterId đó, gửi cho chúng tôi đi bạn ơi!',
   CHAPTER_NOT_EXIST_IN_BOOK: 'Chương này lạ quá, nó không tồn tại trong cuốn truyện này đâu @_@',
-  ERROR_OCCURRED_RETRY_LATER: 'Đã xảy ra lỗi. Bạn ơi! Vui lòng thử lại sau nhé ! <3',
+  ERROR_OCCURRED_RETRY_LATER: 'Đã xảy ra lỗi. \nBạn ơi! Vui lòng thử lại sau nhé ! <3',
   INVALID_CODE: 'Mã không hợp lệ rồi @_@',
 
   // User
-  CREATED_SUGGESTIONED: 'Chúng tôi đã lưu gợi ý của bạn rồi đó! Chờ chúng tôi xử lý nhé!'
+  CREATED_SUGGESTIONED: 'Chúng tôi đã lưu gợi ý của bạn rồi đó! \nChờ chúng tôi xử lý nhé!'
 }
 
 // Sử dụng các hằng số trong mã của bạn
